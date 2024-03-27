7a41354eba0682616ac9c35ae8f34e8ec95110a4
Leangseu Kim <kimleangseu@gmail.com>
2024-03-27 00:23:25 -0400

    fix: update github action (#9)
    * fix: update github action
    
    * chore: add tag format
    
    * chore: update branch target

---
+++ release.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  branches: ['master'], // Adjust this based on your branch setup
+  tagFormat: '${version}',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/exec', // Plugin to execute custom commands
+      {
+        prepareCmd: 'node version-bump.mjs ${nextRelease.version}', // Custom script to run before release
+      },
+    ],
+	[
+		"@semantic-release/github",
+		{
+		  "assets": [
+			{ "path": "main.js", "label": "main.js" },
+			{ "path": "manifest.json", "label": "manifest.json" }
+		  ]
+		},
+	]
+  ],
+};
