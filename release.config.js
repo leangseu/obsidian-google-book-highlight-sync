e46efeb31aa4231b362aaf256df2ba4d0c4add44
leangseu <kimleangseu@gmail.com>
2024-03-26 23:23:45 -0400

    fix: update github action

---
+++ release.config.js
@@ -0,0 +1,24 @@
+// release.config.js
+
+module.exports = {
+  branches: ['lk/wf-test'], // Adjust this based on your branch setup
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/exec', // Plugin to execute custom commands
+      {
+        prepareCmd: 'node version-bump.mjs ${nextRelease.version}', // Custom script to run before release
+      },
+    ],
+	[
+		"@semantic-release/github",
+		{
+		  "assets": [
+			{ "path": "main.js", "label": "main.js" },
+			{ "path": "manifest.json", "label": "manifest.json" }
+		  ]
+		},
+	]
+  ],
+};
--- version-bump.mjs
+++ version-bump.mjs
@@ -1,6 +1,6 @@
 import { readFileSync, writeFileSync } from "fs";
 
-const targetVersion = process.env.npm_package_version;
+const targetVersion = process.argv[2];
 
 // read minAppVersion from manifest.json and bump version to target version
 let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
@@ -12,3 +12,5 @@ writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
 let versions = JSON.parse(readFileSync("versions.json", "utf8"));
 versions[targetVersion] = minAppVersion;
 writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+
+console.log('===== HERE =====')
